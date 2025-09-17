635a6b31d8e3781072e7858ab5332221d5f74a12
Danish <kdanishwins@gmail.com>
2025-09-17 15:23:52 +0530

    commit commit

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
-  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Travel.";
+  const name = "Trravels ss.";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Samantha";
   const defaultTitle =
     process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Travel with Samantha";
-  const defaultDescription = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "Blog about travel and lifestyle.";
+  const defaultDescription =
+    process.env.NEXT_PUBLIC_BLOG_DESCRIPTION ||
+    "Blog about travel and lifestyle.";
 
   return {
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
